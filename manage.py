d1660477a7b5e3919de203529b9afd832fb8810c
Alfred Warui <alfredwarui17@gmail.com>
2021-09-20 03:15:44 +0300

    add run file which is manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,22 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User,Pitches,Comments
+from flask_migrate import Migrate,MigrateCommand
 
 # App instance created
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+#flask-script shell
+@manager.shell
+def make_shell_context(): 
+  return dict(app = app, db = db, User = User, Pitches = Pitches, Comments = Comments)
+
+# Setting up Flask-Migrate to create a new database schema
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 if __name__ == '__main__': 
   manager.run()
