380eefe6133faca8578fbeee75c396566a310a38
Alfred Warui <alfredwarui17@gmail.com>
2021-09-20 03:14:45 +0300

    add app configuration file

--- config.py
+++ config.py
@@ -1,12 +1,25 @@
+import os
+
 class Config: 
   '''
   General configuration class
   '''
+  SECRET_KEY = os.environ.get("SECRET_KEY") #secret key for wtf forms
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://toshiba:@localhost/pitch'
+  UPLOADED_PHOTOS_DEST = 'app/static/photos' #storage location of uploaded photos in the app
+  
+#  app email configurations
+  MAIL_SERVER = 'smtp.googlemail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
+  MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+  MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config): 
   '''
   Production configuration child class
   '''
+  pass
 
 class DevConfig(Config): 
   '''
